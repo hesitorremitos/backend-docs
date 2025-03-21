4c54d8428b9266678f58da6798f95b10ffa99c70
hesitorremitos <hesitorremitos@gmail.com>
2025-03-20 23:04:26 -0400

    feat: Javascript section completed an task in class

--- resources/html/main.js
+++ resources/html/main.js
@@ -1,2 +1,41 @@
-const boton = document.getElementById('btn-form');
-console.log(boton);
+
+
+
+
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const boton = document.querySelector("#btn-form");
+
+const check_email = document.querySelector("#check-email");
+const check_password = document.querySelector("#check-password");
+
+
+function validateEmail() {
+    if (email.value == "") {
+        console.log("Email vacio");
+        check_email.checked = false;
+    } else if (!email.value.includes("@")) {
+        console.log("Email no valido");
+        check_email.checked = false;
+    } else {
+        check_email.checked = true;
+    }
+}
+
+function validatePassword() {
+    if (password.value  == "") {
+        check_password.checked = false;
+        console.log("Contraseña vacia");
+    } else if (password.value.length < 6) {
+        console.log("Contraseña muy corta");
+        check_password.checked = false
+    } else {
+        console.log("Contraseña valida");
+        check_password.checked = true;
+    }
+}
+
+boton.addEventListener("click", function() {
+    validateEmail();
+    validatePassword();
+});
