0a2293de35a02e4ac5139f385b20887ca90f938f
hesitorremitos <hesitorremitos@gmail.com>
2025-06-28 19:11:04 -0400

    feat: Add new practice documentation for Backend course with RESTful API guidelines

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,10 @@ export default defineConfig({
 					autogenerate: { directory: 'solutions' },
 					collapsed: true,
 				},
+				{
+					label: 'Practicas',
+					autogenerate: { directory: 'practicas' },
+				}
 				
 				
 			],
